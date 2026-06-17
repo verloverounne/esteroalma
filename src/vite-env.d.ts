589ac82977eac41be392289b8af7ae9eb8ae2a2e
verloverounne <veronica.seniquel@comunidad.unne.edu.ar>
2026-06-17 11:49:03 -0300

    Add TypeScript declarations for static assets

---
+++ src/vite-env.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="vite/client" />
+
+// Static asset declarations so TypeScript is happy with direct imports
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
