b39fb2927e823ca3e65adf2a1ff5efcec98a48ad
verloverounne <veronica.seniquel@comunidad.unne.edu.ar>
2026-06-18 08:36:46 -0300

    Fix TourVirtual: use openModal API instead of dispatch

--- src/components/TourVirtual.tsx
+++ src/components/TourVirtual.tsx
@@ -6,7 +6,7 @@ import { useModalStore } from '../store/modalStore';
 
 export default function TourVirtual(): JSX.Element {
   const { lang } = useLang();
-  const { dispatch } = useModalStore();
+  const { openModal } = useModalStore();
   const ref = useReveal<HTMLDivElement>();
 
   const content = {
@@ -30,19 +30,18 @@ export default function TourVirtual(): JSX.Element {
         <span className={styles.kicker}>{t.kicker}</span>
         <h2 className={styles.title}>{t.title}</h2>
       </div>
-
       <div className={styles.tourBleed}>
         <div className={styles.aspectWrapper}>
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!4v1717614068565!6m8!1m7!1sCAoSLEFGMVFpcE03Vnd6YkhuX1BfUGpBUnZfTjU0b0NnOTIydml2d3J2Z29Yd0JS!2m2!1d-27.9174152!2d-57.2917711!3f324.7820464878479!4f10!5f0.7820865974627469" 
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!4v1717614068565!6m8!1m7!1sCAoSLEFGMVFpcE03Vnd6YkhuX1BfUGpBUnZfTjU0b0NnOTIydml2d3J2Z29Yd0JS!2m2!1d-27.9174152!2d-57.2917711!3f324.7820464878479!4f10!5f0.7820865974627469"
             className={styles.iframe}
             allowFullScreen
             loading="lazy"
             title="Estero Alma 360 Tour"
           />
-          <button 
+          <button
             className={styles.cta}
-            onClick={() => dispatch({ type: 'OPEN' })}
+            onClick={() => openModal('tour-estero-alma')}
           >
             {t.button}
           </button>
